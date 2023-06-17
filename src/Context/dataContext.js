040f6a8ef5c2941476fc173cf8523f96c5b3276e
Paras Arya <parasarya289@gmail.com>
2023-06-17 22:31:11 +0530

    fix: added netlify.toml, fix page not found issue

--- src/Context/dataContext.js
+++ src/Context/dataContext.js
@@ -11,13 +11,6 @@ import { dataReducer, initDataState } from "../Reducers/dataReducer";
 export const dataContext = createContext();
 export const DataContextProvider = ({ children }) => {
   const [dataState, dataDispatch] = useReducer(dataReducer, initDataState);
-
-  // const wantToRead = books.filter(({ state }) => state === "wantToRead");
-  // const read = books.filter(({ state }) => state === "read");
-  // const currentlyReading = books.filter(
-  //   ({ state }) => state === "currentlyReading"
-  // );
-  // const none = books.filter(({ state }) => state === "none");
   const wantToRead = dataState.books.filter(({ state }) => state === "wantToRead");
   const read = dataState.books.filter(({ state }) => state === "read");
   const currentlyReading = dataState.books.filter(
@@ -29,10 +22,6 @@ export const DataContextProvider = ({ children }) => {
     dataDispatch({
       type: "INIT_DATA",
       payload: {
-        read,
-        wantToRead,
-        currentlyReading,
-        none,
         books,
       },
     });
--- src/Reducers/dataReducer.js
+++ src/Reducers/dataReducer.js
@@ -1,70 +1,26 @@
 export const initDataState = {
-  read: [],
-  none: [],
-  wantToRead: [],
-  currentlyReading: [],
-  searchQuery:"",
-  books:[],
+  searchQuery: "",
+  books: [],
 };
 export const dataReducer = (state, action) => {
   switch (action.type) {
     case "INIT_DATA":
-      return{...state,...action.payload};
+      return { ...state, ...action.payload };
+
     case "UPDATE_BOOKS":
-      return{
-        ...state,
-        books:state.books.map((book)=>book.id===action.payload.id?{...book,state:action.payload.state}:book)
-      }
-    case "MOVE_TO_READ":
       return {
         ...state,
-        read: [...state.read, action.payload.data],
-        none: state.none.filter(({ id }) => id !== action.payload.id),
-        wantToRead: state.wantToRead.filter(
-          ({ id }) => id !== action.payload.id
+        books: state.books.map((book) =>
+          book.id === action.payload.id
+            ? { ...book, state: action.payload.state }
+            : book
         ),
-        currentlyReading: state.currentlyReading.filter(
-          ({ id }) => id !== action.payload.id
-        ),
-        books:state.books.map((book)=>book.id === action.payload.id?{...book,state:action.payload.data.state}:book)
       };
-    case "MOVE_TO_CURRENTLYREADING":
+    case "INIT_SEARCHQUERY":
       return {
         ...state,
-        currentlyReadig: [...state.currentlyReadig, action.payload.data],
-        none: state.none.filter(({ id }) => id !== action.payload.id),
-        wantToRead: state.wantToRead.filter(
-          ({ id }) => id !== action.payload.id
-        ),
-        read: state.read.filter(({ id }) => id !== action.payload.id),
+        searchQuery: action.payload,
       };
-    case "MOVE_TO_WANTTOREAD":
-      return {
-        ...state,
-        wantToRead: [...state.wantToRead, action.payload.data],
-        none: state.none.filter(({ id }) => id !== action.payload.id),
-        read: state.read.filter(({ id }) => id !== action.payload.id),
-        currentlyReading: state.currentlyReading.filter(
-          ({ id }) => id !== action.payload.id
-        ),
-      };
-    case "MOVE_TO_NONE":
-      return {
-        ...state,
-        none: [...state.none, action.payload.data],
-        wantToRead: state.wantToRead.filter(
-          ({ id }) => id !== action.payload.id
-        ),
-        read: state.read.filter(({ id }) => id !== action.payload.id),
-        currentlyReading: state.currentlyReading.filter(
-          ({ id }) => id !== action.payload.id
-        ),
-      };
-      case "INIT_SEARCHQUERY":
-        return{
-          ...state,
-          searchQuery:action.payload
-        }
     default:
       return state;
   }
